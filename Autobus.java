9e3426f360194e49dd6f78b69d81112ebf24ae67
jhovanni223 <jhovanniplatt@gmail.com>
2022-02-16 13:16:28 -0600

    correcion de ejer de Autobus

---
+++ Autobus.java
@@ -0,0 +1,35 @@
+Public class Autobus{
+
+	string marca;
+	strign modelo;
+	string color;
+	int  Cap_pasajeros;
+
+
+ Public Autobus(){
+
+ }
+
+ Public Autobus(strign marca, string modelo, string color, int Cap_pasajeros){
+
+ 	this.marca = marca;
+ 	this.modelo = modelo;
+ 	this.color = color;
+ 	this.Cap_pasajeros = Cap_pasajeros;
+ }	
+
+
+
+Public static void public static void main(String[] args) {
+	string marca = "Mercedes-Benz"; 
+	string modelo = "Boxer" 
+	string color = "Rojo";
+	int Cap_pasajeros = 39 ;
+	Autobus miautobus = new Autobus (marca, modelo , color, Cap_pasajeros);
+
+
+}
+
+}
+
+
